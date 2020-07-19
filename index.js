15abad1d1c479771f5117a7ebb66f9bd8d0aa546
Evan Boswood <evan@boswood.net>
2020-07-18 20:04:05 -0500

    handled adding employees

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const cTable = require("console.table");
 var username = "";
 var password = "";
 
-const departmentsArr = [];
-const employeesArr = [];
-const rolesArr = [];
+var departmentsArr = [];
+var employeesArr = [];
+var rolesArr = [];
 
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "2.0.0.1",
     port: "3306",
-    user: "testing",
-    password: "",
+    user: "evan",
+    password: "Hikbigdlu7320!",
     database: "employee_db"
 });
 
@@ -123,12 +123,35 @@ function addMenu(){
     });
 }
 
-function add(type){
+function updateArrays(){
+    departmentsArr = [];
+    employeesArr = [];
+    rolesArr = [];
+
     connection.query("SELECT name FROM departments",function(err, data){
+        if(err) throw err;
         Object.keys(data).forEach(function(key){
             departmentsArr.push(String(data[key].name));
         });
     });
+
+    connection.query("SELECT first_name FROM employees",function(err, data){
+        if(err) throw err;
+        Object.keys(data).forEach(function(key){
+            employeesArr.push(String(data[key].first_name));
+        });
+    });
+
+    connection.query("SELECT title FROM roles",function(err, data){
+        if(err) throw err;
+        Object.keys(data).forEach(function(key){
+            rolesArr.push(String(data[key].title));
+        });
+    });
+}
+
+function add(type){
+    updateArrays();
 
     const departmentQuestions = [
         {
@@ -190,30 +213,22 @@ function add(type){
                     console.log(response);
                 });
             });
-
-            mainMenu();
             break;
         case "role":
             inquirer.prompt(roleQuestions).then(res => {
-
                 console.log(`New role: ${res.title}\nRole's salary: ${res.salary}\nParent Department: ${res.parentDepartment}`);
-
                 connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${res.title}", ${res.salary}, (SELECT id FROM departments WHERE name = "${res.parentDepartment}"));`, function(err, response){
                     if(err) throw err;
-
                     console.log(response);
                 });
             });
 
-            mainMenu();
             break;
 
         case "employee":
             inquirer.prompt(employeeQuestions).then(res => {
-
+                connection.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("${res.firstname}", "${res.lastname}", (SELECT id FROM roles WHERE title = "${res.role}"), (SELECT id AS m_id FROM employees AS original_table WHERE first_name = "${res.manager}"));`);
             });
-
-            mainMenu();
             break;
     }
 }
@@ -264,5 +279,23 @@ function viewMenu(){
 }
 
 function updateMenu(){
+    var temparray = [];
 
+    connection.query("SELECT first_name FROM employees",function(err, data){
+        if(err) throw err;
+        Object.keys(data).forEach(function(key){
+            temparray.push(String(data[key].first_name));
+        });
+        console.log(temparray);
+    });
+
+    
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employee",
+            message: "Choose an employee to update.",
+            choices: temparray
+        }
+    ]);
 }
